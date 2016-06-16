a2ac7d93582f5404c36a8026576e836f6ef772b2
mark <iamwh1temark@outlook.com>
2016-06-16 10:30:55 +0800

    update setting

---
+++ xueqiu/static/stock/models.py
@@ -0,0 +1,44 @@
+# coding:utf-8
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class Stock(models.Model):
+    stock_number = models.CharField(
+            verbose_name="stock_number",
+            max_length = 20,
+    )
+    name = models.CharField(
+        verbose_name="stock_name",
+        max_length=100
+    )
+    followers_number = models.PositiveIntegerField(
+        verbose_name="followers_number",
+        default=0
+
+    )
+    new_followers_number = models.IntegerField(
+        verbose_name="increased_number",
+        default=0
+    )
+    value_increased_percent = models.CharField(
+        "increase_percent",
+        max_length=10,
+        blank=True,
+        null=True
+    )
+    update_time = models.DateTimeField(
+        'update_time',
+        auto_now=True
+    )
+    
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '股票信息'
+        ordering = ['new_followers_number', 'update_time']
+
